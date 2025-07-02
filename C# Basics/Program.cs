f038aa4acc4b0628fc858efe187ed473b47e4be9
fox <textfox10000@gmail.com>
2025-07-02 20:09:05 +0300

    Problem 01

--- C# Basics/Program.cs
+++ C# Basics/Program.cs
@@ -4,7 +4,40 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region 1. Explain the difference between passing (Value type parameters) by value and by reference then write a suitable c# example.
+            // Value Type 
+            // 1. Passing  by Value [if u change values of the Var in the Function it wont change in the local Var]
+            //int a = 5; int b = 6;
+            //Console.WriteLine(a);// 5
+            //Console.WriteLine(b);// 6
+            //Swap(a, b);
+            //Console.WriteLine(a);// 5
+            //Console.WriteLine(b);// 6
+
+            // 2. Passing  by Ref [if u change values of the Var in the Function it will change in the local Var also]
+
+            //int A = 5; int B = 6;
+            //Console.WriteLine(A);// 5
+            //Console.WriteLine(B);// 6
+            //Swap2(ref A, ref B);
+            //Console.WriteLine(A);// 6
+            //Console.WriteLine(B);// 5
+
+
+
+            #endregion
+        }
+        public static void Swap(int x, int y)
+        {
+            int Temp = x;
+            x = y;
+            y = Temp;
+        }
+        public static void Swap2(ref int x, ref int y)
+        {
+            int Temp = x;
+            x = y;
+            y = Temp;
         }
     }
 }
